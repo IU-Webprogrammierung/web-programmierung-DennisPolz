dd3b66883572d0333393ab2f06844a2825311f8d
DennisPolz <dennis.polz@iubh-fernstudium.de>
2024-11-23 12:18:50 +0100

    Erstellen eines benutzerdefinierten Cursors mit Klickanimation. Anpassen der About-Seite in Bezug auf Barrierefreiheit (Zeilenlänge reduzieren).

---
+++ js/cursor.js
@@ -0,0 +1,13 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("cursorExpand");
+
+    setTimeout(() => {
+        cursor.classList.remove("cursorExpand");
+    }, 500)
+})
